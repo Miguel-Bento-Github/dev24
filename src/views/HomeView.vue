cd59c93d570fc198a15eedb72e6a8ae074c85d62
Miguel Bento <miguel.angelo.f.bento@gmail.com>
2022-01-24 22:01:01 +0100

    Update footer styles

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -55,8 +55,10 @@ const year = new Date().getFullYear();
         {{ content.devices }}
       </p>
     </section>
-    <footer>
-      <small>Created by Dev24 with love 🧡 {{ year }}</small>
+    <footer class="footer">
+      Created by Dev24 with love
+      <span class="footer-emoji">🧡</span>
+      {{ year }}
     </footer>
   </article>
 
@@ -162,6 +164,19 @@ const year = new Date().getFullYear();
       }
     }
   }
+}
+
+.footer {
+  font-size: 1rem;
+  margin-top: 10rem;
+  padding: 1rem;
+  border-radius: 1rem;
+  background: rgba(#000, 0.2);
+}
+
+.footer-emoji {
+  font-size: 1rem;
+  margin-right: 4px;
 }
 
 @keyframes move {
